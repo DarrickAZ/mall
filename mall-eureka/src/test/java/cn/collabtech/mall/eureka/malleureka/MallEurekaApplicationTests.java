bcebb91a66c10cef0a3b5d800396c83603528379
DarrickAZ <zhwwhx@126.com>
2019-11-09 17:58:03 +0800

    ###add for mall-eureka.

---
+++ mall-eureka/src/test/java/cn/collabtech/mall/eureka/malleureka/MallEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.collabtech.mall.eureka.malleureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
