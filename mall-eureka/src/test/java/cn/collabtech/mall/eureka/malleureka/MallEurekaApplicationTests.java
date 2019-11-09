8bbd35183e866369f154546d60c7cfed3519df96
DarrickAZ <zhwwhx@126.com>
2019-11-09 18:09:54 +0800

    ###modify mall-admin.

--- mall-eureka/src/test/java/cn/collabtech/mall/eureka/malleureka/MallEurekaApplicationTests.java
+++ mall-eureka/src/test/java/cn/collabtech/mall/eureka/malleureka/MallEurekaApplicationTests.java
@@ -1,12 +1,8 @@
 package cn.collabtech.mall.eureka.malleureka;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MallEurekaApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
